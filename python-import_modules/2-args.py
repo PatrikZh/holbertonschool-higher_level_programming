477e137b3e5289489b907673072ccc57c9dde1f4
PatrikZh <patrik.zheji@gmail.com>
2023-01-20 07:50:18 -0800

    Adding/Subtracking/Multipli/Division

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     elif arg == 1:
         print("1 argument:")
     else:
-        print("{} arguments:".format(l))
+        print("{} arguments:".format(arg))
     for i in range(1, len(argv)):
         print("{}: {}".format(i, argv[i]))
