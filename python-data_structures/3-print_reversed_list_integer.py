f2bf286af5c1842d3ff293a31157c49bb4a7ca62
PatrikZh <patrik.zheji@gmail.com>
2023-01-23 07:19:25 -0800

    Print in reverse,using reverse function

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    rev_list = my_list[::-1]
-    if rev_list is not None:
-        for num in rev_list:
-            print("{:d}".format(num))
+    if my_list is not None: 
+        reverse_list = my_list[::-1]
+        for num in reverse_list:
+                print("{:d}".format(num))
