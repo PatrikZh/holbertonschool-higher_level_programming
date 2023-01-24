fc25a64ae5bd432e1e647f612aef283c6d0d43a2
PatrikZh <patrik.zheji@gmail.com>
2023-01-24 04:04:35 -0800

    Find and return the biggest number in a list

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list == 0:
-        return None
     max_list = my_list[0]
     for num in my_list:
         if num > len(my_list):
             return num
+    if my_list == 0:
+        return None
