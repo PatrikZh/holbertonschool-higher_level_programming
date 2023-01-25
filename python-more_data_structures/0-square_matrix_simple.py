e4e7c1bb859c49e61414d48f8f14337302125818
PatrikZh <patrik.zheji@gmail.com>
2023-01-25 02:56:10 -0800

    Function that transforms all the values inside matrix in square power, using nested loop and append method

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -3,8 +3,15 @@ def square_matrix_simple(matrix=[]):
     new_matrix = []
     for row in matrix:
         new_row = []
-        for elements in row:
-            elements = elements ** 2
-            new_row.append(elements)
+        for element in row:
+            element = element ** 2 
+            new_row.append(element)
         new_matrix.append(new_row)
     return new_matrix
+
+
+
+    # Another way to do it is :
+# new_matrix = [[value ** 2 for value in row] for row in matrix]
+# return new_matrix
+
