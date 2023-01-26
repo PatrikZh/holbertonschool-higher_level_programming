ed00b0dca6969942aa3521dfa1cc654c85ec6635
PatrikZh <patrik.zheji@gmail.com>
2023-01-26 02:51:55 -0800

    Function that returns common elements

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    return set_1, set_2
+    new_set = set()
+
+    for element in set_1:
+        if element in set_2:
+            new_set.add(element)
+        else:
+            pass
+    return new_set
