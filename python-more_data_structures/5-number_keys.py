5aee035b2213c7cf899c287f13ff5ab9ae35f223
PatrikZh <patrik.zheji@gmail.com>
2023-01-26 03:59:27 -0800

    Return the number of keys in a dictionary -> 2 ways to do it

---
+++ python-more_data_structures/5-number_keys.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    count = 0
+    for key, value in a_dictionary.items():
+            count += 1
+    return count
+
+# count_keys = len(a_dictionary)
+# return count_keys -> it will return the number of keys
