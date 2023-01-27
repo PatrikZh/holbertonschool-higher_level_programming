ed55435d5146a99494b45a45a3b57ab01a1d3d59
PatrikZh <patrik.zheji@gmail.com>
2023-01-27 11:42:07 -0800

    The hardest so far, more info to learnabout it would be great

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman_dictionary = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    roman_dictionary = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
     total = 0
+    if not isinstance(roman_string, str) or roman_string is None:
+        return total
     for i in range(len(roman_string)):
         if roman_string[i] in roman_dictionary:
             current_value = roman_dictionary[roman_string[i]]
