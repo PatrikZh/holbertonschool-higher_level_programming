0f753fe029bfe333630faf2d9812ad4f49013f15
PatrikZh <patrik.zheji@gmail.com>
2023-01-27 09:56:01 -0800

    Function that divides 2 integers and prints the result using try/except

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    division = 0
     try:
-        division = (a / b)
-        if division % 2 ==  0:
-            print("Inside result: 6.0")
-            print("{:d}".format(division))
-        elif division == None:
-            print("Inside result: None")
-            print("{:d}".format(division))
+        division = a / b
     except:
-        pass
+        division = None
     finally:
-        return division
+        print("Inside result: {}".format(division))
