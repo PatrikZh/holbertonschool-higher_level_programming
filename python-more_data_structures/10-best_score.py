3a6997bff3dc5525efecf8b36051b53c49a15bba
PatrikZh <patrik.zheji@gmail.com>
2023-01-27 02:22:44 -0800

    Function that return the biggest key in the dicitonary, usign max_key,max_value/key and value.items() loop method and elementary condition

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -6,6 +6,6 @@ def best_score(a_dictionary):
     max_value = None
     for key, value in a_dictionary.items():
         if max_key is None or value > max_value:
-            max_key = key:
+            max_key = key
             max_value = value
     return max_key
