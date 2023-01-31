5d4c8cd3db9449ba307c791263c2b9d71a17f3c2
PatrikZh <patrik.zheji@gmail.com>
2023-01-31 05:48:40 -0800

    Same thing as 1, but raise errors with messages

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,10 +5,10 @@
 class Square:
     ''' __init__ method foucs on errors'''
     def __init__(self, size=0):
-        self.__self = self
         if type(size) == int:
             True
         else:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+        self.__size = size
