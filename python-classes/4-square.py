559ea4e1d49248442689d5cd4c269a0d5dbc2ed3
PatrikZh <patrik.zheji@gmail.com>
2023-01-31 07:48:32 -0800

    Class square that focuses on getter and setter = get or remove

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,32 +1,38 @@
 #!/usr/bin/python3
+"""Class called square"""
+
+
 class Square:
-        def __init__(self, size=0):
-            if type(size) is int:
-                True
-            else:
-                raise TypeError("size must be an integer")
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            else:
-                pass
-            self.__size = size
+    ''' __init__ method foucs on errors'''
+    def __init__(self, size=0):
+        if type(size) is int:
+            True
+        else:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            pass
+        self.__size = size
 
-        @property
-        def size(self):
-            return self.__size
+    ''' getting the size'''
+    @property
+    def size(self):
+        return self.__size
 
-        @size.setter
-        def size(self, value):
-            if type(value) is int:
-                  True
-            else:
-                raise TypeError("size must be an integer")
-            if value < 0:
-                raise ValueError("size must be >= 0")
-            else:
-                pass    
-            self.__size = value
-        
-        def area(self):
-            return self.__size ** 2
+    ''' setting the new value'''
+    @size.setter
+    def size(self, value):
+        if type(value) is int:
+            True
+        else:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            pass
+        self.__size = value
 
+    ''' method to indicate area'''
+    def area(self):
+        return self.__size ** 2
