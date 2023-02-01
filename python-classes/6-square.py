38cd7b9e59b038a214be4fe66b1d9c611bf8a9ed
PatrikZh <patrik.zheji@gmail.com>
2023-02-01 02:29:23 -0800

    Add a new variable called position just like size

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -15,6 +15,8 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         if len(position) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
+        if type(position[0]) is not int or type(position[1]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
         if position[0] < 0 or position[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = position
@@ -29,6 +31,8 @@ class Square:
         if type(position) is not tuple:
             raise TypeError("position must be a tuple of 2 positive integers")
         if len(position) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(position[0]) is not int or type(position[1]) is not int:
             raise TypeError("position must be a tuple of 2 positive integers")
         if position[0] < 0 or position[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
@@ -54,6 +58,7 @@ class Square:
     def my_print(self):
         if self.__size == 0:
             print()
+            return
         for i in range(self.__position[1]):
             print()
         for my_square in range(self.__size):
