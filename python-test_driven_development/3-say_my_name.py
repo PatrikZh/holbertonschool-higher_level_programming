4ffdb5de3feb8018cd0b9d41c48549c50c9fd26f
PatrikZh <patrik.zheji@gmail.com>
2023-02-02 15:36:34 -0800

    Function that prints first and last name of input, checked by test cases

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+""" Function that prints first&last name"""
 def say_my_name(first_name, last_name=""):
+    ''' Checks type of first&last name if not strings'''
     if type(first_name) != str:
         raise TypeError("first_name must be a string")
     if type(last_name) != str:
