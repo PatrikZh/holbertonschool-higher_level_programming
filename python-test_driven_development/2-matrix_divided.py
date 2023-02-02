2eb3a41b1bb2b5ee60b92f0c51cb75e8451ac5cc
PatrikZh <patrik.zheji@gmail.com>
2023-02-02 14:58:10 -0800

    Functiont that divides all elements in a matrix and stores it in a new one, checked by test cases

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """  Function that stores matrix in a new one divided"""
+
+
 def matrix_divided(matrix, div):
     ''' Create new matrix to store rows as list and elements divided'''
     new_matrix = []
@@ -9,13 +11,15 @@ def matrix_divided(matrix, div):
         raise ZeroDivisionError("division by zero")
     for row in matrix:
         if type(row) is not list:
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError("matrix must be a matrix (list of lists) of "
+                            "integers/floats")
         if len(row) != len(matrix[0]):
             raise TypeError("Each row of the matrix must have the same size")
         new_row = []
         for i in row:
             if type(i) not in [int, float]:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError("matrix must be a matrix (list of lists) of "
+                                "integers/floats")
             new_row.append(round(i / div, 2))
         new_matrix.append(new_row)
     return new_matrix
