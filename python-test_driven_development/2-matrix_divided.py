a76e0edb8234264121a1bbda1e3cdf22b9c109b4
PatrikZh <patrik.zheji@gmail.com>
2023-02-02 07:07:39 -0800

    Just did these

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""  Function that stores matrix in a new one divided"""
+def matrix_divided(matrix, div):
+    ''' Create new matrix to store rows as list and elements divided'''
+    new_matrix = []
+    if type(div) not in [int, float]:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    for row in matrix:
+        if type(row) is not list:
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        if len(row) != len(matrix[0]):
+            raise TypeError("Each row of the matrix must have the same size")
+        new_row = []
+        for i in row:
+            if type(i) not in [int, float]:
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            new_row.append(round(i / div, 2))
+        new_matrix.append(new_row)
+    return new_matrix
