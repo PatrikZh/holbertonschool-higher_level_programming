6c52517c7fb72a6fdba7722d84d1d27ee4cc6496
PatrikZh <patrik.zheji@gmail.com>
2023-02-02 02:01:55 -0800

    Test cases for function that adds integers

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,10 +3,12 @@
 
 
 def add_integer(a, b=98):
+    ''' Checking if type of a, b aren't int or float'''
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
+    ''' If type a, b are float, convert them to int'''
     if type(a) is float:
         a = int(a)
     if type(b) is float:
