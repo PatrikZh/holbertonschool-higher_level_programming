14356a7623f715c9043ea8926f47c6c9ac0aa4d2
PatrikZh <patrik.zheji@gmail.com>
2023-02-02 16:16:23 -0800

    Function that prints 2 newlines if encountered these symbols . , ? and :, checked by test cases

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+""" Function that will print newline after encoutering specified symbols"""
+
+
 def text_indentation(text):
+    ''' Checking and iterating over text for symbols'''
     if type(text) != str:
-        raise TypeError("text must be a string")    
+        raise TypeError("text must be a string")
     for i in text:
         print("{}".format(i), end="")
         if i in ('.', '?', ':'):
