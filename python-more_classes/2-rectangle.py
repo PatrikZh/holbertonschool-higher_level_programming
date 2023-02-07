70e19f4237e03b7f02eb3dc01f7aec5af76f88ad
PatrikZh <patrik.zheji@gmail.com>
2023-02-07 07:51:23 -0800

    Rectangle class that finds perimeter and area

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -34,9 +34,9 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        return self.width * self.height
+        return self.__width * self.__height
 
     def perimeter(self):
-        if self.width == 0 or self.height = 0:
+        if self.__width == 0 or self.__height = 0:
             return 0
-        return (self.width + self.height) * 2
+        return (self.__width + self.__height) * 2
