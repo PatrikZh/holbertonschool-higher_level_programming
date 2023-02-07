2ddc1e092c6356c0237f97838f5a3d0943403f50
PatrikZh <patrik.zheji@gmail.com>
2023-02-07 02:19:58 -0800

    Rectangle created set/get/init

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" Comment do be updated"""
+
+
+class Rectangle:
+    ''' Comment do be updated'''
+    def __init__(self, width=0, height=0):
+        ''' Comment do be updated'''
+        self.__width = width
+        self.__height = height
+    
+    @property
+    def width(self):
+        ''' Comment do be updated'''
+       return self.__width
+    @property
+    def height(self):
+        ''' Comment do be updated'''
+       return self.__height
+
+    @width.setter
+    def width(self):
+        ''' Comment do be updated'''
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+    @height.setter
+    def height(self):
+        ''' Comment do be updated'''
+        if type(height) != int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
