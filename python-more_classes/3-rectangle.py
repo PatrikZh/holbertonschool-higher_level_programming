e979cac2e4f9d5e712ddd7cde4ec2f7473d59d46
PatrikZh <patrik.zheji@gmail.com>
2023-02-08 08:23:22 -0800

    In use the __str__ method, returns a string that was empty

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -44,7 +44,7 @@ class Rectangle:
     def __str__(self):
         string = ""
         if self.__width == 0 or self.__height == 0:
-            return 0
+            return "0"
         for i in range(self.__height):
             for j in range(self.__width):
                 string += '#'
