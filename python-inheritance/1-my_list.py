f3e97c3baac26edb2f5972a329615c6011f41c5e
PatrikZh <patrik.zheji@gmail.com>
2023-02-10 00:20:38 -0800

    To sort you can also use sorted(self) rather then self.sort() only

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+""" Class that creates MyList derived from list"""
+
+
 class MyList(list):
+    ''' Sorting self and storing it in new_list'''
     def print_sorted(self):
         new_list = sorted(self)
         print(new_list)
