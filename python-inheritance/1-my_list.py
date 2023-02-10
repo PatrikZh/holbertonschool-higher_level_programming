a03a52855e900c8e6346fc10c4b643d3fc1beb9e
PatrikZh <patrik.zheji@gmail.com>
2023-02-10 06:14:02 -0800

    Done

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -6,5 +6,4 @@ class MyList(list):
     ''' Sorting self and storing it in new_list'''
     def print_sorted(self):
         new_list = sorted(self)
-        print(new_list)
         return new_list
