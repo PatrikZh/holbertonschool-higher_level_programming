7b5fe474a43ae90758583de9b08ca9bac5532961
PatrikZh <patrik.zheji@gmail.com>
2023-02-10 00:25:25 -0800

    Specify using type() and check

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Function returns True if object is exactly an instance of the specified class else False"""
+""" Function returns True if object is exactly an instance of a_class otherwise False"""
 
 
 def is_same_class(obj, a_class):
