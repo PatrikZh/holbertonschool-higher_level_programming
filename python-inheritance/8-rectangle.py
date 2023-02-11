ad55e95dd49ade187e7b820fdf312ca6ce7c4d87
PatrikZh <patrik.zheji@gmail.com>
2023-02-11 15:58:54 -0800

    Adding more to __init__

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,27 +1,14 @@
 #!/usr/bin/python3
-
-
-''' Creates an class called BaseGeometry'''
+""" Creates an class called BaseGeometry"""
 
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-class BaseGeometry:
+class Rectangle(BaseGeometry):
     ''' Makes Empty class using pass function'''
     def __init__(self, width, height):
-        self.__width = __width
-        self.__height = __height
-
-    ''' Area method that raises error message'''
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    ''' Method that validates value and raises errors'''
-    def integer_validator(self, name, value):
-        if type(value) != int:
-            raise TypeError("<name> must be an integer")
-        if value <= 0:
-            raise ValueError("<name> must be greater than 0")
-        self.name = name
-        self.value = value
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
