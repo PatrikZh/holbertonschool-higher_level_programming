dd6b624c054f634bb85910b30a45d0db8bc113fa
PatrikZh <patrik.zheji@gmail.com>
2023-02-11 17:02:46 -0800

    Class Sqaure that inherits from Rectangle(9-rectangle.py), the __init__ function important

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -3,13 +3,16 @@
 
 
 Rectangle = __import__('9-rectangle').Rectangle
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-class Sqaure(Rectangle):
+class Square(Rectangle):
     ''' Constructor function(init), size must be private'''
     def __init__(self, size):
+        BaseGeometry.integer_validator(self, "size", size)
+        Rectangle.__init__(self, size, size)
         self.__size = size
 
     '''Area function that returns area'''
     def area(self):
-        return self.__size
+        return self.__size ** 2
