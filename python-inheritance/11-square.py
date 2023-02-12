9225e148facd3875af2b383313f788bdfb19d735
PatrikZh <patrik.zheji@gmail.com>
2023-02-11 17:09:50 -0800

    Size, size you can use the same variable to mutliple itslef, iziz and cool

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Creating an class called square"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Square(Rectangle):
+    ''' Constructor function(init), size must be private'''
+    def __init__(self, size):
+        BaseGeometry.integer_validator(self, "size", size)
+        Rectangle.__init__(self, size, size)
+        self.__size = size
+
+    ''' Area function that returns area'''
+    def area(self):
+        return self.__size ** 2
+
+    ''' Str function that returns a string'''
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
