ed321cf6d50fca4fa269390c5e15917fe245d89d
PatrikZh <patrik.zheji@gmail.com>
2023-02-13 08:37:51 -0800

    Setattr sets the key value that wer got from the loop to the object

---
+++ python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Creating class called Student"""
+
+
+class Student:
+    ''' Constructing the required contents'''
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    ''' Method in order to return attributes and values of object(self)'''
+    def to_json(self, attrs=None):
+        result = {}
+        if attrs is None:
+            for key, value in self.__dict__.items():
+                result[key] = value
+        else:
+            for attr in attrs:
+                if attr in self.__dict__:
+                    result[attr] = self.__dict__[attr]
+        return result
+
+    ''' The setattr method sets attributes using 2 arguments'''
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            setattr(self, key, value)
+
