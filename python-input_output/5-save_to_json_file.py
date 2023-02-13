9d872dc4a994e2021005bc40f3874dfd774502db
PatrikZh <patrik.zheji@gmail.com>
2023-02-13 05:13:44 -0800

    Use the with statement first to write an Object to a text file, then convet to JSON file string format

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Function uses with statement and then converts to JSON"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    ''' Write authorituy to f and then convert to json file format'''
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(json.dumps(my_obj))
