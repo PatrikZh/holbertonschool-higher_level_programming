eebeeee1f5045e962ea2ebb3490aa45120071a8b
PatrikZh <patrik.zheji@gmail.com>
2023-02-13 04:58:27 -0800

    .loads() function used to decode Python data structures to and from JSON strings, respectively

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Function .loads() to convert from json format to data structure"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    ''' .loads() function used'''
+    return json.loads(my_str)
