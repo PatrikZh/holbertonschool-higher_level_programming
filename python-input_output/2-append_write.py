093e20ed260c19094352e8f79b0b6dee1d481bee
PatrikZh <patrik.zheji@gmail.com>
2023-02-13 04:36:23 -0800

    a stand for append mode

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""  Append method argument"""
+
+
 def append_write(filename="", text=""):
+    ''' Mode a stands for appending'''
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
