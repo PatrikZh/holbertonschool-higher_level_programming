05c69b956db78237c7210a217147fae9343c533a
PatrikZh <pzheji@actirana.edu.al>
2023-02-14 07:06:28 -0800

    Checks if id exits, add object id to id of class, if not increment the variable and added to the id

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-""" """
+""" Class called Base"""
+
 
 class Base:
-    
+    ''' Init function that has two conditions, if id exists or not'''
     __nb_objects = 0
+
     def __init__(self, id=None):
         if id is not None:
-            self.id = id        
+            self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
