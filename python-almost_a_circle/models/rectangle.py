6b1f2ff89103bec44e9657322bf116cca037be1f
PatrikZh <pzheji@actirana.edu.al>
2023-02-15 02:49:08 -0800

    Add condition to setter and getter

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -9,39 +9,55 @@ class Rectangle(Base):
     ''' Constructor method that takes all arguments and validates'''
     def __init__(self, width, height, x=0, y=0, id=None):
         Base.__init__(self, id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
         return self.__width
 
+    @width.setter
+    def width(self, width):
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = width
+
     @property
     def height(self):
         return self.__height
 
-    @width.setter
-    def width(self, width):
-        self.__width = width
-
     @height.setter
     def height(self, height):
+        if type(height) != int:
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
         self.__height = height
 
     @property
     def x(self):
         return self.__x
 
+    @x.setter
+    def x(self, x):
+        if type(x) != int:
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = x
+
     @property
     def y(self):
         return self.__y
 
-    @x.setter
-    def x(self, x):
-        self.__x = x
-
     @y.setter
     def y(self, y):
+        if type(y) != int:
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
         self.__y = y
