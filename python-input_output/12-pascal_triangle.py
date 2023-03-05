7631a19426bdaffae9089f6fe858aabefd92e52b
PatrikZh <patrik.zheji@gmail.com>
2023-03-05 11:38:35 -0800

    A lttle gibberish

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,17 @@
+#!/bin/bash
+""" Pascal triangle format build beneath"""
+
+
+def pascal_triangle(n):
+    ''' Making an pascal triangle'''
+    triangle = []
+    for i in range(n):
+        row = [1]
+        prev_row = []
+        if i > 0:
+            prev_row = triangle[i - 1]
+            for j in range(len(prev_row) - 1):
+                row.append(prev_row[j] + prev_row[j + 1])
+            row.append(1)
+        triangle.append(row)
+    return triangle
