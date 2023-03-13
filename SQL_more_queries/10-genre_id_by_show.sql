d9ed4b5bbc6657da0574ae9068709d377fd7072c
PatrikZh <patrik.zheji@gmail.com>
2023-03-12 17:13:31 -0700

    Listing shows in sql through title and genre id

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,2 @@
 -- Listing shows contained in hbtn_0d_tvshows that have at least one genre linked
-SELECT tv_shows.title, tv_show_genres.genre_id FROM hbtn_0d_tvshows INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title, tv_show_genres.genre_id;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tvshows INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title, tv_show_genres.genre_id;
