1daf0d816a45a425b9a7906ebe7be0926f75d221
PatrikZh <patrik.zheji@gmail.com>
2023-03-13 04:16:27 -0700

    LEFT JOIN returns null if right is empty

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,2 +1,2 @@
 -- Listing shows contained in hbtn_0d_tvshows that have at least one genre linked
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id IF genre.id NOT EXISTS SET genre.id = NULL ORDER BY tv_shows.title, tv_show_genres.genre_id; 
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title, tv_show_genres.genre_id; 
