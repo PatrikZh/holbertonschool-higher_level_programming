5e2ffb6e1533cb2ad777b96c93388301ac203f0b
PatrikZh <patrik.zheji@gmail.com>
2023-03-13 05:03:19 -0700

    Mistake on mispelling

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,2 +1,2 @@
 -- Lisiting all genres from hbtn_0d_tvshows and displays the number of shows linked to each
-SELECT tv_genres.name AS genre, COUNT(tv_shows_genres.genre.id) AS number_of_shows FROM tv_genres LEFT JOIN tv_shows_genres ON tv_genres.id = tv_shows_genres.genre_id GROUP BY tv_genres.genre.id ORDER BY number_of_shows DESC;
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows FROM tv_genres JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id GROUP BY tv_genres.id ORDER BY number_of_shows DESC;
