9039ee4c0e14212462f9d96f1ab262629ca61509
PatrikZh <patrik.zheji@gmail.com>
2023-03-16 04:18:56 -0700

    ORM that lists all states from the database hbtn_0e_0_usa

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,16 +4,21 @@ import MySQLdb
 import sys
 
 
-mydb = MySQLdb.connect(
-    host = "localhost",
-    user = sys.argv[1],
-    password = sys.argv[2],
-    database = sys.argv[3]
-)
-cursor = mydb.cursor()
-query = "SELECT * FROM states ORDER BY id"
-cursor.execute(query)
-results = cursor.fetchall()
-for row in results:
-    print(row)
-mydb.close()
+if __name__ == "__main__":
+    mysql_user = sys.argv[1]
+    mysql_password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=mysql_user,
+                         passwd=mysql_password,
+                         db=db_name)
+
+    cursor = db.cursor()
+    query = "SELECT * FROM states ORDER BY id"
+    cursor.execute(query)
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+    db.close()
