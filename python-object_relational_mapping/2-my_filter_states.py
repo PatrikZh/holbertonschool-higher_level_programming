dc370a43a5980c1382f3d44b9eb1dc83d36cdfbd
PatrikZh <patrik.zheji@gmail.com>
2023-03-16 05:08:49 -0700

    Takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Script that takes an argument and returns name where matches the argument"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    mysql_user = sys.argv[1]
+    mysql_password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    db.MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=mysql_user,
+            passwd=mysql_password,
+            db=db_name)
+    
+    cursor = db.cursor()
+    query = "SELECT * FROM states WHERE name LIKE '{}%' ORDER BY id ASC".format(search_term)
+    cursor.execute(query)
+    result = cursor.fetchall()
+    for el in result:
+        print(el)
+    db.close()
