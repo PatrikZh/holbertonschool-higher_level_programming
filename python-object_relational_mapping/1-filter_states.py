b04d2b5ebf9f77165a48284e98f4b736e275e7f3
PatrikZh <patrik.zheji@gmail.com>
2023-03-16 04:36:29 -0700

    Only states that start with Uppercase letter N. Use LIKE 'N%' method

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Listing states with a name starting with N from the database hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    mysql_user = sys.argv[1]
+    mysql_password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=mysql_user,
+            passwd=mysql_password,
+            db=db_name)
+
+    cursor = db.cursor()
+    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id"
+    cursor.execute(query)
+    result = cursor.fetchall()
+    for el in result:
+        print(el)
+    db.close()
