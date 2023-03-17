652183e12f6f7d934d7bf60d3693777cba1d0285
PatrikZh <patrik.zheji@gmail.com>
2023-03-17 04:22:12 -0700

    Update

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Listing name of a state as an argument and lists all cities"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    mysql_user = sys.argv[1]
+    mysql_password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=mysql_user
+            passwd=mysql_password,
+            db=db_name)
+    cursor = db.cursor()
+    query = "SELECT cities.* FROM cities \
+    JOIN states ON cities.state_id = states.id \
+    WHERE states.name=%s ORDER BY cities.id"
+    cursor.execute(query)
+    result = cursor.fetchall()
+    for el in result:
+        print(el)
+    db.close()
