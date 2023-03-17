99b6e53db54523c564858e8a8d5ba4e35a23a817
PatrikZh <patrik.zheji@gmail.com>
2023-03-17 04:23:33 -0700

    Update

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(
             host="localhost",
             port=3306,
-            user=mysql_user
+            user=mysql_user,
             passwd=mysql_password,
             db=db_name)
     cursor = db.cursor()
