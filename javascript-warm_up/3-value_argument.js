1def629c2e290a97eac8327f0f1c6e59390ac4f2
PatrikZh <patrik.zheji@gmail.com>
2023-04-19 06:32:03 -0700

    Just pushing done it locally before

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const args = process.argv.slice(2)
 
-if (args.length == 0) {
+if (args == 0) {
 	console.log('No argument');
 } else if (process.argv[2]) {
 	console.log(process.argv[2]);
