49c076f0a6952731dac1544ae4a3dd084bbf042b
PatrikZh <patrik.zheji@gmail.com>
2023-04-20 05:55:41 -0700

    Use dot notation when acessing objects values

---
+++ javascript-warm_up/12-object.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+
+if (myObject) {
+	if (myObject.value === 12) {
+		myObject.value = 89;
+		console.log(myObject);
+	}
+}
