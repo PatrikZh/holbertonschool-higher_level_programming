9e74daf8f2f59a9cfc4d51b84a4a41da49df94f0
PatrikZh <patrik.zheji@gmail.com>
2023-04-20 07:59:30 -0700

    add

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -2,14 +2,10 @@
 function add (a, b) {
   const args = process.argv.slice(2);
   if (!a) {
-    a = args[0];
-  } else {
-    a = 3;
-  }
+    a = args[0] || 3;
+  } 
   if (!b) {
-    b = args[1];
-  } else {
-    b = 5;
+    b = args[1] || 5;
   }
   return parseInt(a) + parseInt(b);
 }
