85525176985c72fcaf8060ddf48f2cdb7f896172
PatrikZh <patrik.zheji@gmail.com>
2023-04-19 17:43:05 -0700

    Factorial

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+function factorial (num) {
+  if (num === 1) {
+    return 1;
+  }
+  return num * factorial(num - 1);
+}
+
+const num = parseInt(process.argv[2]);
+console.log(factorial(num));
