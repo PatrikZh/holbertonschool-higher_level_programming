db67b552e238a36b32b0bb38125480c5b23593e3
PatrikZh <patrik.zheji@gmail.com>
2023-04-23 19:38:53 -0700

    Latin is Natin'

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request(url, (error, response, body) => {
+  if (error) throw error;
+  fs.writeFile(filePath, body, 'utf-8', (err) => {
+    if (err) throw err;
+    console.log(`The response was saved to filePath`);
+  });
+});
