9c8c31073e70bb8aa9615f2dc5ec543d4a17c99f
PatrikZh <patrik.zheji@gmail.com>
2023-04-23 19:32:03 -0700

    Done

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const request = require('request');
+
+const movieId = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  }
+
+  if (response.statusCode !== 200) {
+    console.error('Unexpected status code: response.statusCode');
+  }
+
+  const movie = JSON.parse(body);
+  console.log(movie.title);
+});
