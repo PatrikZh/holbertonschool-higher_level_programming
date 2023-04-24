b0233460a22ded0afd0a573c6b3502b3baec68f3
PatrikZh <patrik.zheji@gmail.com>
2023-04-23 18:29:58 -0700

    Done before, just pushing in

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log('code:', response.statusCode);
+  }
+});
